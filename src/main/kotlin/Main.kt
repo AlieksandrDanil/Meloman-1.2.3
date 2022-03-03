a4abf477897a63684b490abcf453ebbd2accd134
AlieksandrDanil <inspcrimea@gmail.com>
2022-03-03 17:31:06 +0300

    Added code in file Main (MelomanDiscount)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
 fun main() {
-    println("Run Meloman")
+    val previousPurchasesSum = 12_400
+    val isPermanentUser = true
+    val nowPurchasesSum: Int = if (previousPurchasesSum in 1_001..10_000) {
+        previousPurchasesSum - 100
+    } else if (previousPurchasesSum >= 10_001) {
+        previousPurchasesSum - (previousPurchasesSum * 5 / 100)
+    } else {
+        previousPurchasesSum
+    }
+    val result = if (isPermanentUser) nowPurchasesSum - (nowPurchasesSum * 1 / 100) else nowPurchasesSum
+    println("Постоянный покупатель музыки, сумма со скидкой $result рублей")
 }
